fab0e166bfe956e4d6fa3d410ef49fa8d8cc1c04
OswaldoHDZ <oswaldo.hernandez.juarez@gmail.com>
2023-05-23 12:50:27 -0600

    Base de datos

--- src/main/java/com/especialidad/bd/WsProyectoEspecialidadBdIiuacmApplication.java
+++ src/main/java/com/especialidad/bd/WsProyectoEspecialidadBdIiuacmApplication.java
@@ -1,18 +1,28 @@
 package com.especialidad.bd;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@PropertySource(value = "file:${UACM_WS}/WSProyectoEspecialidadBdIIUACM.properties", ignoreResourceNotFound = true)
 public class WsProyectoEspecialidadBdIiuacmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsProyectoEspecialidadBdIiuacmApplication.class, args);
 	}
-	
+	@Autowired
+	 private Environment env;
 	@Bean
 	  public WebMvcConfigurer corsConfigurer() {
 	    return new WebMvcConfigurer() {
@@ -26,5 +36,9 @@ public class WsProyectoEspecialidadBdIiuacmApplication {
 	      }
 	    };
 	  }
-
+	@Bean
+	@Qualifier("sqlserver")
+		public DataSource dataSourceSqlServerAdmin() throws NamingException {
+		return (DataSource) new JndiTemplate().lookup(env.getProperty("DSSMSSQLDSSQLSERVER"));
+	}
 }
